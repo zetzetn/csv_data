63989bef416c79e9485208813f82e4f27b379063
tani <taniguchitakayasu@tani-Mac-book-pro.local>
2020-02-09 11:51:35 +0900

    クラス名修正

--- db/migrate/20200208131453_create_users.rb
+++ db/migrate/20200208131453_create_users.rb
@@ -1,3 +1,4 @@
+=begin
 class CreateDataTests < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
@@ -7,6 +8,17 @@ class CreateDataTests < ActiveRecord::Migration[5.2]
       t.integer :age
       t.string :address
 
+      t.timestamps
+    end
+  end
+end
+=end
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.integer :age
+      t.string :address
       t.timestamps
     end
   end
